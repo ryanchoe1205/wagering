a5461ec1c43d7c06bfabce4dc641a0946cd8e15a
Joshua Cole <jColeChanged@gmail.com>
2013-03-29 20:51:45 -0700

    Added south.

--- wagering/settings.py
+++ wagering/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'wagers',
     'django.contrib.admin',
+    'south',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
