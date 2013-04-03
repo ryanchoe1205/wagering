163371f1a6d772b98fc849a17bd37bda8ad7f451
Joshua Cole <jColeChanged@gmail.com>
2013-04-03 08:08:30 -0700

    Prop visibility now proportional to how likely users are to want to interact with them.

--- wagers/urls.py
+++ wagers/urls.py
@@ -1,12 +1,10 @@
-from wagers.views import WagerView, WagerPayoutView, WagerDeleteView, WagerCreateView, ResetEverythingView, WagerOpenView, WagerCloseView
+from wagers.views import WagerView, WagerPayoutView, WagerDeleteView, WagerCreateView, ResetEverythingView, WagerOpenView, WagerCloseView, WagerListView
 from django.conf.urls import patterns, url
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import permission_required, login_required
 from django.views.generic.list import ListView
 from wagers.models import Wager
 from django.contrib.auth.models import User
 
-class WagerListView(ListView):
-    model = Wager
 
 
 class UserListView(ListView):
@@ -17,7 +15,7 @@ class UserListView(ListView):
 #url(r'^wagers/delete/', WagerDeleteView.as_view())
 urlpatterns = patterns('',
     url(r'^wagers/tourneyreset', permission_required("wagers.delete_wager")(ResetEverythingView.as_view())),
-    url(r'^wagers/index/', WagerListView.as_view(), name="wager-list"),
+    url(r'^wagers/index/', login_required(WagerListView.as_view())),
     url(r'^wagers/delete/', permission_required("wagers.delete_wager")(WagerDeleteView.as_view())),
     url(r'^wagers/add/', permission_required("wagers.add_wager")(WagerCreateView.as_view())),
     url(r'^wagers/open/', permission_required("wagers.change_wager")(WagerOpenView.as_view())),
