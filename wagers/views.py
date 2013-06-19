e12d69185faa3e34449565d2a1389cd6442a8bb5
Joshua Cole <jColeChanged@gmail.com>
2013-06-18 20:22:41 -0700

    Moved the user interface seen by users to one click.

--- wagers/views.py
+++ wagers/views.py
@@ -243,10 +243,6 @@ class ChangeSchedule(View):
             return redirect("tournament-admin", tourney_uuid)
         else:
             return HttpResponseForbidden("You can't do that.")
-            
-
-
-
 
 class AddProposition(View):
     """
